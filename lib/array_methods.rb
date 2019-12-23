ade9201924147ff17da1854d2e1738018837f491
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-23 20:53:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,9 @@
-# def using_include(array, element)
+def using_include(array, element)
 array_element = ["A", "B", "C"]
 array.element.include?("D")
+end 
+
+
 array_element.sort
 array_element.reverse
 array_element.first
