e9bf23c4fab6b519ca9381a399d38a920a0b2c1e
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-23 20:55:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,30 +3,23 @@ array_element = ["A", "B", "C"]
 array.element.include?("D")
 end 
 
-
+def using_sort
 array_element.sort
-array_element.reverse
-array_element.first
-array_element.last
-array_element.size
-#
-# def using_sort(array)
-#
-# end
-#
-# def using_reverse(array)
-#
-# end
-#
+end 
 
-# def using_first(array)
-#
-# end
-#
-# def using_last(array)
-#
-# end
-#
-# def using_size(array)
-#
-# end
+def using_reverse
+array_element.reverse
+end 
+
+def using_first
+array_element.first
+end 
+
+def using_last
+array_element.last
+end 
+
+def using_size
+array_element.size
+end 
+
